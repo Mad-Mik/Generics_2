552106f7f2547ecdff9b964840409f695e8776f8
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-15 01:09:48 +0400

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2
+{
+    public class MyList<T>
+    {
+        private T[] array = Array.Empty<T>();
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public int Count { get { return array.Length; } }
+
+        public void Add(T value)
+        {
+            var newArray = new T[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[array.Length] = value;
+
+            array = newArray;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(5);
+            myList.Add(7);
+            myList.Add(9);
+            myList.Add(45);
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
